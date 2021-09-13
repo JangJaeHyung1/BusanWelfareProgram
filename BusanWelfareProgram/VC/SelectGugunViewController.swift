a45ffc3e9ea9321fe7d756a6f339d378ded38dfe
JangJaeHyung1 <ggassoon@naver.com>
2021-09-13 21:06:02 +0900

    각 vc 생성

---
+++ BusanWelfareProgram/VC/SelectGugunViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectGugunViewController.swift
+//  BusanWelfareProgram
+//
+//  Created by jh on 2021/09/13.
+//
+
+import UIKit
+
+class SelectGugunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
